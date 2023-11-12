53bcc8c96c084b2c92bd4434c1a9b59a2904184b
Alma Ordoñez <alma.ordonez2002@gmail.com>
2023-11-12 00:21:10 -0600

    Corrigiendo

--- src/main/java/com/project/RestAppAWS/dto/model/AlumnoData.java
+++ src/main/java/com/project/RestAppAWS/dto/model/AlumnoData.java
@@ -14,7 +14,7 @@ public class AlumnoData {
     @NotBlank
     private String apellidos;
     @Pattern(regexp = "^[a-zA-Z]+\\d+$", message = "La matrícula no cumple con el formato adecuado, debe comenzar con una letra y posteriormente al menos un número")
-    private int matricula;
+    private String matricula;
     @DecimalMin(value = "0.0", inclusive = true, message = "Error: Ingresar un valor mayor a 0")
     @DecimalMax(value = "100.0", inclusive = true, message = "Error: Ingresar un valor menor o igual a 100")
     private  double promedio;
